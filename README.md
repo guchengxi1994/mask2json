d8b246538e8d384019d9a09a22fb7bf039b2d367
guchengxi1994 <528490652@qq.com>
2020-06-10 09:48:32 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
+<!--
+ * @lanhuage: markdown
+ * @Descripttion: 
+ * @version: beta
+ * @Author: xiaoshuyui
+ * @Date: 2020-06-09 16:23:03
+ * @LastEditors: xiaoshuyui
+ * @LastEditTime: 2020-06-10 09:47:43
+--> 
 # mask2json
  mask imgs to labelme jsons,and so on
+
+ This script is used to convert mask-labels to json files for [labelme](https://github.com/wkentaro/labelme).
+
+ Also ,json files to masks can be found on this [site](https://blog.csdn.net/gaoyi135/article/details/103870646). Sometimes there will be an ERROR,should add this [file](./backup/draw.py) in labelme path ('../anaconda/Lib/site-packages/labelme/utils ' my path) and add some codes in \_\_init__.py:
+
+    from .draw import label_colormap
+    from .draw import _validate_colormap
+    from .draw import label2rgb
+    from .draw import draw_label
+    from .draw import draw_instances 
+
+Here shows an example using this script.
+
+origin image
+
+![img1](./static/1-2cvt.jpg)
+
+mask image
+
+![img1](./static/1-2cvt.png)
+
+manually_labeled image
+
+![img1](./backup/manually_labeled.png)
+
+auto_labeled image
+
+![img1](./backup/auto_labeled.png)
+![img1](./backup/auto_labeled_ori.png)
+
+Also ,for [labelimg](https://github.com/tzutalin/labelImg),a small tool to convert results(yolo) to [xml-files](./utils/img2xml). (I tested [single object](./utils/img2xml/processor_singleObj.py) but didn't test [multi objects](./utils/img2xml/processor_multiObj.py)
+
+
--- mask2json.py
+++ mask2json.py
@@ -5,19 +5,68 @@
 @Author: xiaoshuyui
 @Date: 2020-06-09 16:24:12
 @LastEditors: xiaoshuyui
-@LastEditTime: 2020-06-09 17:34:20
+@LastEditTime: 2020-06-10 09:22:51
 '''
 import cv2
 from utils import getShape
 from utils import img2base64
+from utils import rmQ
+import json
+import os
 
-if __name__ == "__main__":
+def test():
     img = "D:\\testALg\mask2json\mask2json\static\\1-2cvt.png"
+    oriImg = "D:\\testALg\mask2json\mask2json\static\\1-2cvt.jpg"
+    imgShape = cv2.imread(img).shape
 
     region = getShape.process(img)
     # print(type(region))
     # print(region.shape)
-
+    points = []
+    shapes = []
     for i in range(0,region.shape[0]):
         print(region[i][0])
+        points.append(region[i][0].tolist())
+
+    obj = dict()
+    obj['version'] = '4.2.9'
+
+    obj['flags'] = {}
+
+    shape = dict()
+    shape['label'] = 'weld'  #whatever other label
+    shape['points'] = points
+    shape['group_id']='null'
+    shape['shape_type']='polygon'
+    shape['flags']={}
+
+
+    shapes.append(shape)
+
+    obj['shapes'] = shapes
+    obj['imagePath'] = oriImg.split(os.sep)[-1]
+    obj['imageData'] = str(img2base64.imgEncode(oriImg))
+    obj['imageHeight'] = imgShape[0]
+    obj['imageWidth'] = imgShape[1]
+
+
+    j = json.dumps(obj,sort_keys=True, indent=4)
+
+    print(j)
+
+    with open('D:\\testALg\mask2json\mask2json\static\\1-2cvt.json','w') as f:
+        f.write(j)
+
+    
+    rmQ.rm('D:\\testALg\mask2json\mask2json\static\\1-2cvt.json')
+
+            
+
+
+
+
+if __name__ == "__main__":
+    test()
+
+
 

4878222413d2631b01c9d6a1614d8242c7d697b9
guchengxi1994 <528490652@qq.com>
2020-07-10 09:28:13 +0800

    test

---
+++ test_img2xml.py
@@ -0,0 +1,20 @@
+'''
+@lanhuage: python
+@Descripttion:  img2xml test script
+@version: beta
+@Author: xiaoshuyui
+@Date: 2020-07-10 09:11:39
+@LastEditors: xiaoshuyui
+@LastEditTime: 2020-07-10 09:20:15
+'''
+from utils.img2xml import processor_singleObj
+
+if __name__ == "__main__":
+    """
+    will cause some error on Windows \n
+    such as the file or dirname starts with 't' or 'n' or numbers \n
+    """ 
+
+    f = open("./test_img2xml",'w')
+    f.writelines(processor_singleObj.img2xml("test","aa","test\\test.xx",12,23,"aaa",123,444,4523,664))
+    f.close()

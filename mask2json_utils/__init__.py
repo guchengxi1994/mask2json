501e0b158d77d73543ce766c2da5c3990d71e2df
guchengxi1994 <528490652@qq.com>
2020-08-20 09:45:10 +0800

    0.3.2

--- mask2json_utils/__init__.py
+++ mask2json_utils/__init__.py
@@ -5,9 +5,9 @@
 @Author: xiaoshuyui
 @Date: 2020-07-10 10:09:56
 LastEditors: xiaoshuyui
-LastEditTime: 2020-08-19 17:19:34
+LastEditTime: 2020-08-20 09:40:36
 '''
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 __appname__ = 'convertmask'
 

bdc239b2936c589f0f3540ea369a54cd9b91d28a
guchengxi1994 <528490652@qq.com>
2020-09-03 08:03:26 +0800

    xml2mask

---
+++ convertmask/utils/xml2mask/x2m.py
@@ -0,0 +1,17 @@
+'''
+lanhuage: python
+Descripttion: 
+version: beta
+Author: xiaoshuyui
+Date: 2020-09-03 07:58:48
+LastEditors: xiaoshuyui
+LastEditTime: 2020-09-03 08:02:16
+'''
+
+import xml.etree.ElementTree as ET
+import os
+import glob
+from tqdm import tqdm
+from convertmask.utils.methods.logger import logger
+
+

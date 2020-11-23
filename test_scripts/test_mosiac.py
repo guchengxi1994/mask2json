f91bd975590b6d3fee0fd3fbbfa9f3ec0137d6b7
guchengxi1994 <528490652@qq.com>
2020-11-23 15:29:35 +0800

    re-write mosaic.py
    add 2 functions:
    getBoxes and mosiac_img_no_reshape.
    test several times,maybe bugs.

--- test_scripts/test_mosiac.py
+++ test_scripts/test_mosiac.py
@@ -5,7 +5,7 @@ version: beta
 Author: xiaoshuyui
 Date: 2020-10-26 08:31:13
 LastEditors: xiaoshuyui
-LastEditTime: 2020-11-20 13:58:05
+LastEditTime: 2020-11-23 10:00:41
 '''
 import sys
 
@@ -13,10 +13,9 @@ sys.path.append("..")
 import os
 
 from skimage import io
-from convertmask.utils.auglib.optional.mosaic import mosiac_img,mosiacScript,mosiac_img_no_reshape
+from convertmask.utils.auglib.optional.mosaic import mosiac_img, mosiacScript, mosiac_img_no_reshape, mosiacScript_no_reshape
 
-BASE_DIR = os.path.abspath(os.path.dirname(
-        os.getcwd())) + os.sep + 'static'
+BASE_DIR = os.path.abspath(os.path.dirname(os.getcwd())) + os.sep + 'static'
 
 if __name__ == "__main__":
     BASE_DIR = os.path.abspath(os.path.dirname(
@@ -29,6 +28,12 @@ if __name__ == "__main__":
     # io.imsave(imgPath,mosicImg)
 
     # mosiacScript([oriImg,],[xmlPath,],BASE_DIR,True)
-    
-    mosicImg,_ = mosiac_img_no_reshape([oriImg,oriImg,oriImg,oriImg],heightFactor=0.4, widthFactor=0.3)
-    io.imsave(imgPath,mosicImg)
+
+    # mosicImg, _ = mosiac_img_no_reshape([oriImg, oriImg, oriImg, oriImg],
+    #                                     heightFactor=0.4,
+    #                                     widthFactor=0.3)
+    # io.imsave(imgPath, mosicImg)
+
+    mosiacScript_no_reshape([oriImg, oriImg, oriImg, oriImg],
+                            [xmlPath, xmlPath, xmlPath, xmlPath], BASE_DIR,
+                            True)

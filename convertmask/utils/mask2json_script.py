b0fca9ea9447f1d484af587326c02f96e3dc028e
guchengxi1994 <guchengxi1994@qq.com>
2021-10-30 16:31:07 +0800

    Update mask2json_script.py

--- convertmask/utils/mask2json_script.py
+++ convertmask/utils/mask2json_script.py
@@ -13,7 +13,7 @@ import os
 
 from tqdm import tqdm
 
-from convertmask.utils.methods import getMultiShapes
+from convertmask.utils.methods import get_multi_shapes
 from convertmask.utils.methods.logger import logger
 
 
@@ -29,7 +29,7 @@ def getJsons(imgPath, maskPath, savePath, yamlPath=''):
     logger.info("currently, only *.jpg supported")
 
     if os.path.isfile(imgPath):
-        getMultiShapes.getMultiShapes(imgPath, maskPath, savePath, yamlPath)
+        get_multi_shapes.getMultiShapes(imgPath, maskPath, savePath, yamlPath)
 
     elif os.path.isdir(imgPath):
         oriImgs = glob.glob(imgPath + os.sep + '*.jpg')
@@ -38,7 +38,7 @@ def getJsons(imgPath, maskPath, savePath, yamlPath=''):
             i_mask = i.replace(imgPath, maskPath)
             if os.path.exists(i_mask):
                 # print(i)
-                getMultiShapes.getMultiShapes(i, i_mask, savePath, yamlPath)
+                get_multi_shapes.getMultiShapes(i, i_mask, savePath, yamlPath)
             else:
                 logger.warning('corresponding mask image not found!')
                 continue
@@ -52,7 +52,7 @@ def getXmls(imgPath, maskPath, savePath):
     logger.info("currently, only *.jpg supported")
 
     if os.path.isfile(imgPath):
-        getMultiShapes.getMultiObjs_voc(imgPath, maskPath, savePath)
+        get_multi_shapes.getMultiObjs_voc(imgPath, maskPath, savePath)
     elif os.path.isdir(imgPath):
         oriImgs = glob.glob(imgPath + os.sep + '*.jpg')
         maskImgs = glob.glob(maskPath + os.sep + '*.jpg')
@@ -61,7 +61,7 @@ def getXmls(imgPath, maskPath, savePath):
             i_mask = i.replace(imgPath, maskPath)
             # print(i)
             if os.path.exists(i_mask):
-                getMultiShapes.getMultiObjs_voc(i, i_mask, savePath)
+                get_multi_shapes.getMultiObjs_voc(i, i_mask, savePath)
             else:
                 logger.warning('corresponding mask image not found!')
                 continue
